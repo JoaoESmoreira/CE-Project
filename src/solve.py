1e3cce7de107f8d187dfa3fc46f1ed544dc4b677
JoaoESmoreira <jesmoreira2010@gmail.com>
2024-04-03 18:00:40 +0100

    add a visualize the solution and a solve (another way to do the evolutionary algorithm

---
+++ src/solve.py
@@ -0,0 +1,105 @@
+
+import random
+
+
+mmap = []
+n = None
+with open("./data/MAP_8_BY_8/input01.txt", "r") as f:
+    n = int(f.readline())
+    for _ in range(n):
+        mmap.append(f.readline()[:-1])
+num_actions = list(range(4))
+
+
+population_size = 100
+num_generations = 200
+elite_percentage = 0.2
+mutation_rate = 0.1
+
+
+def mapping(individual) -> list[tuple[int, int]]:
+    last_position = (0, 0)
+    path = []
+    for component in individual:
+        if component == 0:
+            last_position = (last_position[0], last_position[1]-1)
+        elif component == 1:
+            last_position = (last_position[0]+1, last_position[1])
+        elif component == 2:
+            last_position = (last_position[0], last_position[1]+1)
+        elif component == 3:
+            last_position = (last_position[0]-1, last_position[1])
+        path.append(last_position)
+    return path
+
+def evaluate_individual(individual) -> int:
+    path = mapping(individual)
+    count = 0
+    for step in path:
+        for i in step:
+            if i < 0 or i == n:
+                count += 1
+    for step in path:
+        if 0 <= step[0] < n and 0 <= step[1] < n and mmap[step[0]][step[1]] == 'H':
+            count += 1
+
+    fitness = 0
+    if count == 0:
+        fitness = len(individual)
+        fitness = path[-1][0] + path[-1][0]
+        if path[-1] == (n-1, n-1):
+            fitness *= 10
+    else:
+        fitness = -count
+    return fitness
+
+def initialize_population() -> list[list[int]]:
+    return [[random.randint(0, 3) for _ in range(random.randint(1, 100))] for _ in range(population_size)]
+
+def select_parents(population, fitness_scores, elite_percentage) -> list[list[int]]:
+    sorted_indices = sorted(range(len(fitness_scores)), key=lambda i: fitness_scores[i], reverse=True)
+    elite_size = int(elite_percentage * len(population))
+    elite_indices = sorted_indices[:elite_size]
+    
+    return [population[idx] for idx in elite_indices]
+
+def crossover(parent1, parent2) -> tuple[list[int], list[int]]:
+    crossover_point = random.randint(0, min(len(parent1), len(parent2)))
+    child1 = parent1[:crossover_point] + parent2[crossover_point:]
+    child2 = parent2[:crossover_point] + parent1[crossover_point:]
+    return child1, child2
+
+def mutate(individual, mutation_rate) -> list[int]:
+    for i in range(len(individual)):
+        if random.random() < mutation_rate:
+            individual[i] = random.randint(0, 3)
+    if random.random() < mutation_rate:
+        individual.append(random.randint(0, 3))
+    return individual
+
+
+if __name__ == "__main__":
+    population = initialize_population()
+    for generation in range(num_generations):
+        fitness_scores = [evaluate_individual(individual) for individual in population]
+
+        best_individual_index = fitness_scores.index(max(fitness_scores))
+        best_individual = population[best_individual_index]
+        best_fitness = fitness_scores[best_individual_index]
+
+        print(f"Generation {generation+1}, Best Fitness: {best_fitness}", best_individual)
+
+        parents = select_parents(population, fitness_scores, elite_percentage)
+        offspring = []
+        while len(offspring) < population_size:
+            parent1, parent2 = random.sample(parents, 2)
+            child1, child2 = crossover(parent1, parent2)
+            child1 = mutate(child1, mutation_rate)
+            child2 = mutate(child2, mutation_rate)
+            offspring.extend([child1, child2])
+        population = offspring
+    
+    # Evaluate the best individual
+    best_individual_fitness = evaluate_individual(best_individual)
+    print("Best Individual Actions:", best_individual)
+    print("Best Individual Fitness:", best_individual_fitness)
---
+++ src/visualize_solution.py
@@ -0,0 +1,32 @@
+
+import gymnasium as gym
+from gymnasium.envs.toy_text.frozen_lake import generate_random_map
+import random
+
+map_4_by_4 = generate_random_map(size=4, seed=42)
+map_8_by_8 = generate_random_map(size=8, seed=42)
+map_12_by_12 = generate_random_map(size=12, seed=42)
+
+MAX_ITERATIONS_4_by_4 = 100
+MAX_ITERATIONS_8_by_8 = 200
+MAX_ITERATIONS_12_by_12 = 500
+
+if __name__ == "__main__":
+    fenotype = [1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2]
+    fenotype = [1, 1, 1, 1, 2, 2, 1, 1, 2, 1, 2, 2, 2, 2]
+    my_map = map_8_by_8
+
+    RENDER_MODE = 'human'
+    env = gym.make('FrozenLake-v1', desc=my_map, is_slippery=False, render_mode=RENDER_MODE)
+    observation, info = env.reset(seed=42)
+    if RENDER_MODE is not None:
+        env.render()
+
+    for action in fenotype:
+        observation, reward, terminated, truncated, info = env.step(action)
+        print(action, observation, reward, terminated, truncated, info)
+        #print(action, observation, reward, terminated, truncated, info)
+        if RENDER_MODE is not None:
+            env.render()
+        if terminated:
+            break
