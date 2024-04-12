a7b381f92c7cb2ab3b98493b67eef92f480b619b
JoaoESmoreira <jesmoreira2010@gmail.com>
2024-04-12 19:00:02 +0100

    finish the qtable representation

--- src/solve3.py
+++ src/solve3.py
@@ -16,11 +16,12 @@ individual_size = 200
 elite_size = int(elite_percentage * population_size)
 
 
-def fenotype(grid) -> list[int]:
+def fenotype1(grid) -> list[int]:
     individual = []
     visited = set()
     position = (0,0)
     component = None
+    print(grid)
     while True:
         if grid[position[0]][position[1]] == 0:
             if position[1] != 0:
@@ -44,7 +45,17 @@ def fenotype(grid) -> list[int]:
         individual.append(component)
     return individual
 
-def mapping(grid):
+def fenotype(path):
+    individual = []
+    position = (0, 0)
+    actions = {(0, -1):0, (1, 0):1, (0,1):2, (-1,0):3}
+    for step in path:
+        action = (step[0] - position[0], step[1] - position[1])
+        individual.append(actions[action])
+        position = step
+    return individual
+
+def mapping(grid) -> list[tuple[int, int]]:
     individual = []
     visited = set()
     position = (0,0)
@@ -62,6 +73,7 @@ def mapping(grid):
             if position[0] != 0:
                 position = (position[0]-1, position[1])
         if position == (n-1, n-1) or position in visited:
+            individual.append(position)
             break
         visited.add(position)
         individual.append(position)
@@ -75,7 +87,7 @@ def evaluate_individual(path) -> int:
     
     if len(path) == 0:
         return -1000000
-    return (path[-1][0] + path[-1][1])*2 - (abs(path[-1][0] - (n+1)) + abs(path[-1][1] - (n+1))) - len(path)
+    return (path[-1][0] + path[-1][1])*2 - (abs(path[-1][0] - (n+1)) + abs(path[-1][1] - (n+1)))# - len(path)
 
 def is_feasible(path) -> bool:
     for step in path:
@@ -126,57 +138,63 @@ def two_point_crossover(parent1, parent2) -> tuple[list[int], list[int]]:
     child2 = parent2[:point1] + parent1[point1:point2] + parent2[point2:]
     return child1, child2
 
+def uniform_crossover(parent1, parent2) -> tuple[list[list[int]], list[list[int]]]:
+    child1 = [[0 for _ in range(n)] for _ in range(n)]
+    child2 = [[0 for _ in range(n)] for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            if random.random() < 0.5:
+                child1[i][j] = parent1[i][j]
+                child2[i][j] = parent2[i][j]
+            else:
+                child1[i][j] = parent2[i][j]
+                child2[i][j] = parent1[i][j]
+    return child1, child2
+
+def heuristic_mutation(individual) -> list[list[int]]:
+    individual[n-1][n-2] = 2
+    individual[n-2][n-1] = 1
+    return individual
+
 def mutate(individual, mutation_rate) -> list[int]:
-    for i in range(len(individual)):
-        if random.random() < mutation_rate:
-            individual[i] = random.randint(1, 2)
-    if random.random() < 0.8 and len(individual) < individual_size:
-        path = mapping(individual)
-        if len(path) > 0:
-            number_steps = min(abs(path[-1][0] - n), abs(path[-1][1] - n))
-            for i in range(random.randint(1, number_steps)):
-                individual.append(random.randint(1, 2))
-        else:
-            for i in range(n):
-                individual.append(random.randint(1, 2))
+    for i in range(n):
+        for j in range(n):
+            if random.random() < mutation_rate:
+                individual[i][j] = random.randint(1, 2)
     return individual
 
 def sea():
     population = initialize_population()
-    fitness_scores = [evaluate_individual(individual) for individual in population]
-    best_individual_index = fitness_scores.index(max(fitness_scores))
-    best_individual = fenotype(population[best_individual_index])
-    best_fitness = fitness_scores[best_individual_index]
 
-    print(fitness_scores)
-    print(best_individual)
-    print(best_fitness)
-    #for generation in range(num_generations):
-    #    fitness_scores = [evaluate_individual(individual) for individual in population]
-    #    best_individual_index = fitness_scores.index(max(fitness_scores))
-    #    best_individual = population[best_individual_index]
-    #    best_fitness = fitness_scores[best_individual_index]
+    for generation in range(num_generations):
+        paths          = [mapping(individual) for individual in population]
+        fitness_scores = [evaluate_individual(path) for path in paths]
 
-    #    offspring = [[random.randint(0, 3) for _ in range(random.randint(1, 100))] for _ in range(10)]
-    #    while len(offspring) < population_size:
-    #        parent1, parent2 = tornement_selection(population, fitness_scores)
-    #        if random.random() < crossover_rate:
-    #            child1, child2 = tow_point_crossover(parent1, parent2)
-    #        else:
-    #            child1, child2 = parent1, parent2
-    #        if random.random() < mutation_rate:
-    #            child1 = mutate(child1, mutation_rate)
-    #        if random.random() < mutation_rate:
-    #            child2 = mutate(child2, mutation_rate)
-    #        offspring.extend([child1, child2])
-    #    population = offspring
-    #best_individual_fitness = evaluate_individual(best_individual)
-    #if is_feasible(best_individual):
-    #    print("--- Feasible Individual Actions: {fitness}".format(fitness=best_individual_fitness), best_individual)
-    #    return True
-    #else:
-    #    print("Not Feasible Individual Actions: {fitness}".format(fitness=best_individual_fitness), best_individual)
-    #    return False
+        best_individual_index = fitness_scores.index(max(fitness_scores))
+        best_map        = paths[best_individual_index]
+        best_fitness    = fitness_scores[best_individual_index]
+        best_individual = population[best_individual_index]
+
+        offspring = [best_individual]
+        while len(offspring) < population_size:
+            parent1, parent2 = tornement_selection(population, fitness_scores)
+            if random.random() < crossover_rate:
+                child1, child2 = uniform_crossover(parent1, parent2)
+            else:
+                child1, child2 = parent1, parent2
+            if random.random() < mutation_rate:
+                child1 = mutate(child2, mutation_rate)
+            if random.random() < mutation_rate:
+                child2 = mutate(child2, mutation_rate)
+            child1, child2 = heuristic_mutation(child1), heuristic_mutation(child2)
+            offspring.extend([child1, child2])
+        population = offspring
+    if is_feasible(best_map):
+        print("--- Feasible Individual Actions: {fitness}".format(fitness=best_fitness), fenotype(best_map))
+        return True
+    else:
+        print("Not feasible Individual Actions: {fitness}".format(fitness=best_fitness), fenotype(best_map))
+        return False
 
 def random_with_random_restart():
     best_fitness = -100000
@@ -189,28 +207,32 @@ def random_with_random_restart():
         fitness_scores = [evaluate_individual(path) for path in paths]
 
         current_best_individual_index = fitness_scores.index(max(fitness_scores))
-        current_best_individual       = population[current_best_individual_index]
-        current_best_fitness          = fitness_scores[current_best_individual_index]
-        current_best_path             = paths[current_best_individual_index]
+        current_best_individual  = population[current_best_individual_index]
+        current_best_fitness     = fitness_scores[current_best_individual_index]
+        current_best_path        = paths[current_best_individual_index]
 
         if best_individual is None or best_fitness < current_best_fitness:
             best_map        = current_best_path
             best_fitness    = current_best_fitness
             best_individual = current_best_individual
-
+    best_individual[3][2] = 2
+    best_individual[2][3] = 1
+    best_map = mapping(best_individual)
+    best_fitness = evaluate_individual(best_map)
     if is_feasible(best_map):
-        print("--- Feasible Individual Actions: {fitness}".format(fitness=best_fitness), fenotype(best_individual))
+        print("--- Feasible Individual Actions: {fitness}".format(fitness=best_fitness), fenotype(best_map))
         return True
     else:
-        print("Not feasible Individual Actions: {fitness}".format(fitness=best_fitness), fenotype(best_individual))
+        print("Not feasible Individual Actions: {fitness}".format(fitness=best_fitness), fenotype(best_map))
         return False
 
 
 if __name__ == "__main__":
-    PATH_MAP = "./data/MAP_4_BY_4/input02.txt"
-    with open(PATH_MAP, "r") as f:
-        n = int(f.readline())
-        mmap = []
-        for _ in range(n):
-            mmap.append(f.readline()[:-1])
-    random_with_random_restart()
+    for i in range(10):
+        PATH_MAP = "./data/MAP_12_BY_12/input0{i}.txt".format(i=i)
+        with open(PATH_MAP, "r") as f:
+            n = int(f.readline())
+            mmap = []
+            for _ in range(n):
+                mmap.append(f.readline()[:-1])
+        sea()
